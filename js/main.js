bd3615a010274327cf0379ab518ffc3cd002a923
BrianLM <brian.distefano@libertymutual.com>
2017-08-29 07:53:58 -0700

    Created Game Logic

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-console.log("Up and running!");
+var cardsInPlay =[];
+var cards = ["queen","queen","king","king"];
+var cardOne= cards[0];
+cardsInPlay.push(cardOne);
+var cardTwo =cards[1];
+console.log("User Flipped " + cardOne);
+cardsInPlay.push(cardTwo);
+console.log("User Flipped " + cardTwo);
+if(cardsInPlay[0]===cardsInPlay[1]){
+	alert("You found a match!");
+
+}
+else{
+	alert("Sorry, try again.")
+}
